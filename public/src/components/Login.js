47592406813e7ba192a43782b4fe689b00444ce0
lipeishang <1315789454@qq.com>
2017-05-29 18:59:32 +0800

    add  redux

---
+++ public/src/components/Login.js
@@ -0,0 +1,15 @@
+/**
+ * Created by lipeishang on 17-5-29.
+ */
+import React, {Component} from 'react';
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div>Hello World!</div>
+        )
+    }
+}
+//导出组件
+
+export default Login;
---
+++ public/src/containers/Login.js
@@ -0,0 +1,4 @@
+/**
+ * Created by lipeishang on 17-5-29.
+ */
+import {connect} from 'react-redux';
