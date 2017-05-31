6900782f05a8989dfc13984ade85e51c8a13bdbb
code <1165444511@qq.com>
2017-05-31 14:15:33 +0800

    show something in play

--- public/src/components/Play.js
+++ public/src/components/Play.js
@@ -1,9 +1,25 @@
-import React ,{Component} from 'react';
+import React, {Component} from "react";
 
-export default class Play extends Component{
-    render(){
+export default class Play extends Component {
+    render() {
         return <div>
-            Play
+            <div className="operate">
+                <div>影片:<input type="text"/>
+                    <button>查询</button>
+                    <button>增加</button>
+                </div>
+            </div>
+            <div className="playInfo">
+                <table>
+                    <th>id</th>
+                    <th>影片名称</th>
+                    <th>时长</th>
+                    <th>类型</th>
+                    <th>主演</th>
+                    <th>价格</th>
+                    <th>操作</th>
+                </table>
+            </div>
         </div>
     }
 }
