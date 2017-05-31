8abae4edd773bd72321312b741a5ec1da5314cc9
code <1165444511@qq.com>
2017-05-31 17:39:12 +0800

    merge

--- server/models/Studios.js
+++ server/models/Studios.js
@@ -2,13 +2,9 @@ import mongoose  from 'mongoose';
 mongoose.Promise = require('bluebird');
 
 const Studios = mongoose.model('Studios',{
-<<<<<<< HEAD
-    _id:Number,
-    cul:Number,
-=======
+
     id:Number,
     col:Number,
->>>>>>> 1c3f29d234646e2a25afb6678970c9a57f3a2764
     row:Number,
 });
 
