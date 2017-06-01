0cbc5bf9db14c598d8e4a5ef5e85e3b4a8ffbb83
lipeishang <1315789454@qq.com>
2017-06-01 17:52:55 +0800

    add link

--- public/src/components/Nav.js
+++ public/src/components/Nav.js
@@ -17,7 +17,7 @@ export default class Nav extends Component {
 
                     </div>
                     <span className="token">
-                        <Link to="/userCenter" className={username?'':'hidden'}>{username}</Link>
+                        <Link to="/userCenter" className={username ? '' : 'hidden'}>{username}</Link>
                         </span>
                 </div>
             </div>
@@ -26,32 +26,25 @@ export default class Nav extends Component {
                     <div className="col-md-2">
                         <ul id="main-nav" className="nav nav-tabs nav-stacked">
                             <li className="">
-                                <a href="#">
+                                <Link to="/studios">
                                     <span className="studiomanage"> </span>
                                     演出厅管理
-                                </a>
+                                </Link>
                             </li>
                             <li>
-                                <a href="#systemSetting" className="nav-header collapsed" data-toggle="collapse">
+                                <Link to="/seat" className="nav-header collapsed" data-toggle="collapse">
                                     座位管理
-                                </a>
-                                {/*<ul id="systemSetting" className="nav nav-list collapse secondmenu">*/}
-                                {/*<li><a href="#"><i className="glyphicon glyphicon-user"></i>用户管理</a></li>*/}
-                                {/*<li><a href="#"><i className="glyphicon glyphicon-th-list"></i>菜单管理</a></li>*/}
-                                {/*<li><a href="#"><i className="glyphicon glyphicon-asterisk"></i>角色管理</a></li>*/}
-                                {/*<li><a href="#"><i className="glyphicon glyphicon-edit"></i>修改密码</a></li>*/}
-                                {/*<li><a href="#"><i className="glyphicon glyphicon-eye-open"></i>日志查看</a></li>*/}
-                                {/*</ul>*/}
+                                </Link>
                             </li>
                             <li>
-                                <a href="./plans.html">
+                                <Link to="/employees">
                                     员工管理
-                                </a>
+                                </Link>
                             </li>
                             <li>
-                                <a href="./grid.html">
+                                <Link to="/play">
                                     剧目管理
-                                </a>
+                                </Link>
                             </li>
                             <li>
                                 <a href="./charts.html">
